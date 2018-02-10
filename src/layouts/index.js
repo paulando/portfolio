f13e33b7353b6ec294a27e6b03d31c5a4b9f622f
Paulius <andriekus.paulius@gmail.com>
2018-02-10 21:34:01 +0200

    added twitter meta tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,21 +6,31 @@ import './index.scss'
 import '../styles/_template.scss'
 
 import favicon from '../../favicon.ico'
-import og_image from '../modules/index/images/Paulius-Andriekus.jpg'
+import meta_image from '../modules/index/images/Paulius-Andriekus.jpg'
+
+const meta_url = 'https://andriekuspaulius.com'
+const meta_title = 'Front-end web developer | Paulius Andriekus'
+const meta_description = 'Personal website of front-end web developer - Paulius Andriekus.'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
       title="Front-end web developer | Paulius Andriekus"
       meta={[
-        { name: 'description', content: 'Personal website of front-end web developer - Paulius Andriekus.' },
+        { name: 'description', content: meta_description },
         { name: 'keywords', content: 'Front-end, Web, Developer, Programmer, Coder, Javascript, PHP, ReactJS' },
         { property: 'fb:app_id', content: '167713340527355' },
-        { property: 'og:url', content: 'https://andriekuspaulius.com' },
+        { property: 'og:url', content: meta_url },
         { property: 'og:type', content: 'website' },
-        { property: 'og:title', content: 'Front-end web developer | Paulius Andriekus' },
-        { property: 'og:description', content: 'Personal website of front-end web developer - Paulius Andriekus.' },
-        { property: 'og:image', content: `https://andriekuspaulius.com/${og_image}` },
+        { property: 'og:title', content: meta_title },
+        { property: 'og:description', content: meta_description },
+        { property: 'og:image', content: `${meta_url}/${meta_image}` },
+        { property: 'twitter:card', content: 'summary' },
+        { property: 'twitter:site', content: '@PaulAndriekus' },
+        { property: 'twitter:creator', content: '@PaulAndriekus' },
+        { property: 'twitter:title', content: meta_title },
+        { property: 'twitter:description', content: meta_description },
+        { property: 'twitter:image', content: `${meta_url}/${meta_image}` }
       ]}
       link={[
         { rel: 'icon', href: favicon },
